1203fa8c73d18d1ec814284da2ab845241faddb1
Rogier van Straten <rogier@from89.com>
2025-11-15 22:14:02 +0100

    fix: rm jest

--- __fixtures__/changed-project-directories.ts
+++
@@ -1,6 +0,0 @@
-import { jest } from '@jest/globals'
-
-export const getChangedProjectPaths =
-  jest.fn<
-    typeof import('../src/changed-project-directories').getChangedProjectPaths
-  >()
--- __fixtures__/node.ts
+++
@@ -1,7 +0,0 @@
-import { jest } from '@jest/globals'
-import { execSync as _execSync } from 'node:child_process'
-
-// Mock the child_process module
-// jest.mock('node:child_process')
-
-export const execSync = jest.fn<typeof _execSync>()
